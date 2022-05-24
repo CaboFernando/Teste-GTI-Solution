77478be914f95385e072f419840526d13d4d3f61
carlos.santos <carlos.santos@sp.agence.com.br>
2022-05-24 11:57:13 -0300

    Criando as models no projeto API e add as dependências do EF 6

---
+++ ProjetoAPI/Models/Cliente.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace ProjetoAPI.Models
+{
+    [Table("Clientes")]
+    public class Cliente
+    {
+        [Key]
+        public int ClienteId { get; set; }
+
+        [Required, StringLength(14)]
+        public string Cpf { get; set; }
+
+        [Required, StringLength(100)]
+        public string Nome { get; set; }
+
+        [StringLength(14)]
+        public string Rg { get; set; }
+
+        public DateTime DataExpedicao { get; set; }
+
+        public string OrgaoExpedicao { get; set; }
+
+        public string UfExpedicao { get; set; }
+
+        [Required]
+        public DateTime DataNascimento { get; set; }
+
+        [Required]
+        public string Sexo { get; set; }
+
+        [Required]
+        public string EstadoCivial { get; set; }
+
+        [Required]
+        public virtual Endereco Endereco { get; set; }
+
+        public virtual int EnderecoId { get; set; }
+    }
+}
---
+++ ProjetoAPI/Models/Endereco.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace ProjetoAPI.Models
+{
+    [Table("Enderecos")]
+    public class Endereco
+    {
+        [Key]
+        public int EnderecoId { get; set; }
+
+        [Required, StringLength(150)]
+        public string Logradouro { get; set; }
+
+        [Required, StringLength(100)]
+        public string Numero { get; set; }
+
+        [StringLength(100)]
+        public string Complemento { get; set; }
+
+        [Required, StringLength(100)]
+        public string Bairro { get; set; }
+
+        [Required, StringLength(100)]
+        public string Cidade { get; set; }
+
+        [Required, StringLength(100)]
+        public string Uf { get; set; }
+    }
+}

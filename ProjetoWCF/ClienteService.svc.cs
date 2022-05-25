e6156c1fa283d9a373ab1572c95af975c7658ea3
carlos.santos <carlos.santos@sp.agence.com.br>
2022-05-25 16:07:05 -0300

    Criando ProjetoWCF na solução Fazendo a conexão com novo banco dbGTI-WCF criado de acordo com dbGTI-API do outro projeto

---
+++ ProjetoWCF/ClienteService.svc.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace ProjetoWCF
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "ClienteService" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select ClienteService.svc or ClienteService.svc.cs at the Solution Explorer and start debugging.
+    public class ClienteService : IClienteService
+    {
+        private Entities _db;
+
+        public Cliente Delete(int id)
+        {
+            using (_db = new Entities())
+            {
+                _db.Configuration.ProxyCreationEnabled = false;
+                Cliente cliente = _db.Set<Cliente>().Find(id);
+                _db.Set<Cliente>().Remove(cliente);
+                _db.SaveChanges();
+
+                return cliente;
+            }
+        }
+
+        public Cliente Delete(Cliente cliente)
+        {
+            using (_db = new Entities())
+            {
+                _db.Configuration.ProxyCreationEnabled = false;
+                _db.Set<Cliente>().Remove(cliente);
+                _db.SaveChanges();
+
+                return cliente;
+            }
+        }
+
+        public Cliente Find(int id)
+        {
+            using (_db = new Entities())
+            {
+                _db.Configuration.ProxyCreationEnabled = false;
+                Cliente cliente = _db.Clientes.Single(x => x.ClienteId == id);
+
+                return cliente;
+            }
+        }
+
+        public List<Cliente> FindAll()
+        {
+            using (_db = new Entities())
+            {
+                _db.Configuration.ProxyCreationEnabled = false;
+                List<Cliente> clientes = _db.Clientes.Include("Endereco").ToList();
+
+                return clientes;
+            }
+        }
+
+        public Cliente New(Cliente cliente)
+        {
+            using (_db = new Entities())
+            {
+                _db.Configuration.ProxyCreationEnabled = false;
+                _db.Clientes.Add(cliente);
+                _db.SaveChanges();
+
+                return cliente;
+            }
+        }
+
+        public Cliente Update(Cliente cliente)
+        {
+            using (_db = new Entities())
+            {
+                _db.Configuration.ProxyCreationEnabled = false;
+                _db.Entry(cliente).State = EntityState.Modified;
+                _db.SaveChanges();
+
+                return cliente;
+            }
+        }
+    }
+}
---
+++ ProjetoWCF/IClienteService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace ProjetoWCF
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IClienteService" in both code and config file together.
+    [ServiceContract]
+    public interface IClienteService
+    {
+        [OperationContract]
+        List<Cliente> FindAll();
+
+        [OperationContract]
+        Cliente Find(int id);
+
+        [OperationContract]
+        Cliente New(Cliente cliente);
+
+        [OperationContract]
+        Cliente Update(Cliente cliente);
+
+        [OperationContract]
+        Cliente Delete(int id);
+
+        [OperationContract]
+        Cliente Delete(Cliente cliente);
+    }
+}

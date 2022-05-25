05c50c307c8b1b0a92e7f9d539fd71483617367c
carlos.santos <carlos.santos@sp.agence.com.br>
2022-05-25 16:31:48 -0300

    Adicionando serelização das dependências

--- ProjetoWCF/IClienteService.cs
+++ ProjetoWCF/IClienteService.cs
@@ -3,7 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.Serialization;
 using System.ServiceModel;
+using System.ServiceModel.Channels;
+using System.ServiceModel.Description;
+using System.ServiceModel.Dispatcher;
 using System.Text;
+using System.Xml;
 
 namespace ProjetoWCF
 {
@@ -12,9 +16,11 @@ namespace ProjetoWCF
     public interface IClienteService
     {
         [OperationContract]
+        [ReferencePreservingDataContractFormat]
         List<Cliente> FindAll();
 
         [OperationContract]
+        [ReferencePreservingDataContractFormat]
         Cliente Find(int id);
 
         [OperationContract]
@@ -28,5 +34,34 @@ namespace ProjetoWCF
 
         [OperationContract]
         Cliente Delete(Cliente cliente);
+    }
+
+    public class ReferencePreservingDataContractFormatAttribute : Attribute, IOperationBehavior
+    {
+        public void AddBindingParameters(OperationDescription description, BindingParameterCollection parameters) { }
+
+        public void ApplyClientBehavior(OperationDescription description, ClientOperation proxy)
+        {
+            IOperationBehavior innerBehavior = new ReferencePreservingDataContractSerializerOperationBehavior(description);
+            innerBehavior.ApplyClientBehavior(description, proxy);
+        }
+        public void ApplyDispatchBehavior(OperationDescription description, DispatchOperation dispatch)
+        {
+            IOperationBehavior innerBehavior = new ReferencePreservingDataContractSerializerOperationBehavior(description);
+            innerBehavior.ApplyDispatchBehavior(description, dispatch);
+        }
+        public void Validate(OperationDescription description) { }
+
+    }
+
+    public class ReferencePreservingDataContractSerializerOperationBehavior : DataContractSerializerOperationBehavior
+    {
+
+        public ReferencePreservingDataContractSerializerOperationBehavior(OperationDescription operationDescription) : base(operationDescription) { }
+
+        public override XmlObjectSerializer CreateSerializer(Type type, XmlDictionaryString name, XmlDictionaryString ns, IList<Type> knownTypes)
+        {
+            return new DataContractSerializer(type, name, ns, knownTypes, 2147483646, false, true, null);
+        }
     }
 }
